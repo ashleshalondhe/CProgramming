c779b6a1ad0bb357ff6daa1a3e012f3c92815566
Ashlesha Londhe <140420012+ashleshalondhe@users.noreply.github.com>
2023-08-03 19:46:30 +0530

    Add files via upload

---
+++ 02_Program to accept user input/02_Program to accept user input.c
@@ -0,0 +1,29 @@
+// Program to accept user input- name, age, salary
+
+#include <stdio.h>       //* link section
+
+int main()              //* Main Function     
+{
+    char name[20];
+    int age;
+    float sal;
+    
+    printf("\nEnter name: ");   // Output Function Program
+    scanf("%s", name);         //  Input Function Program
+    
+    printf("\nEnter age: ");   
+    scanf("%s", name);
+    
+    printf("\nEnter Salary: ");   
+    scanf("%s", name);
+    
+    printf("\nName = %s",name);
+    
+    printf("\nAge = %d",age);
+    
+    printf("\nSalary = %0.2f",sal);
+    
+    return 0;
+}
+
+
